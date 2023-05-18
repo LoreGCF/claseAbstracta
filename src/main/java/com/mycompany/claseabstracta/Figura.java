6369a6a8d9eb2bf1c5219370008161027c3c04ea
Lorena Garcia <g_fgl@live.com.ar>
2023-05-18 09:52:21 -0300

    ejercicio clase abstracta

---
+++ src/main/java/com/mycompany/claseabstracta/Figura.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.claseabstracta;
+
+
+public abstract class Figura {
+    
+    protected double x;
+    protected double y;
+
+    protected Figura() {
+    }
+
+    protected Figura(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    
+    
+    
+    public abstract double calcularArea();
+    
+}
